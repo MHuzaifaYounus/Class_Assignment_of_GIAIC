4761226e6b4241c2a2ee0431a16bf862f4ab5436
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-11-06 08:34:45 +0500

    completed assignment 5

---
+++ quarter2/assignmant5/app/page.tsx
@@ -0,0 +1,29 @@
+
+import Header from "@/components/header";
+import Image from "next/image";
+import {Libre_Bodoni} from "next/font/google";
+
+const mainFont = Libre_Bodoni({
+  weight: "400",
+});
+export default function Home() {
+  return (
+    <div className="main w-full">
+      <Header />
+      <div className="hero flex justify-evenly items-center w-full h-[825.56px]">
+        <div className={`hero-left flex flex-col justify-evenly w-1/2 h-[573.59] ${mainFont.className}`}>
+          <h1 className={`font-bold text-[37.84px] w-[472.97] `}>IMPECCABLE CRAFTSMANSHIP AND FINESSE</h1>
+          <p className="w-[789.9px] font-medium text-[29.24px] text-primary">An example of intricate workmanship and detail, elegant necklaces and long and short chains form a part of our desirable collection.</p>
+          <button className="w-[247.67px] h-[49.2px] rounded-[8.6px] p-[8.6px] bg-primary font-medium text-[25.8px] leading-[32.25px] text-white">Explore Now</button>
+
+        </div>
+        <div className="hero-right overflow-hidden relative flex items-center justify-center w-[421.38px] h-[573.59] bg-primary rounded-tl-[128.99px] rounded-br-[128.99px]">
+          <Image className="absolute top-0 left-0" src="/slider.png" alt="no img found" width={421.38} height={573.59} />
+          <div className="layer w-[380.1px] h-[525.43px] rounded-tl-[128.99px] rounded-br-[128.99px] border-[0.86px] border-white z-10"></div>
+
+
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ quarter2/assignmant5/components/header.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import React from 'react'
+import {Rye} from "next/font/google";
+
+
+
+const logoFont = Rye({
+  weight: "400",
+});
+
+
+const Header = () => {
+  return (
+    <div className="header items-center justify-evenly w-full h-[135px] bg-primary flex ">
+        <div className="logo w-[242.56px] h-[87.45px] flex">
+          <Image src="/logo.svg" alt="no img found" width={63.52} height={70.35} />
+          <h1 className={`${logoFont.className} text-[66.78px] text-white`}>urity</h1>
+        </div>
+        <div className="searchbar flex justify-evenly items-center h-[56.24px] w-[769.89px] bg-white rounded-[13.83px]">
+          <input className="w-[90%] h-[80%]" type="search" placeholder="Search for Gold Jewellery, Diamond Jewellery and more…" />
+          <Image src="/search.svg" alt="no img found" width={21.5} height={21.5} />
+
+        </div>
+        <div className="icons h-[30.08px] w-[177.8px] flex justify-between items-center">
+          <Image src="/heart.svg" alt="no img found" width={33.42} height={29.71} />
+          <Image src="/profile.svg" alt="no img found" width={28.97} height={29.28} />
+          <Image src="/cart.svg" alt="no img found" width={38.01} height={38.08} />
+
+        </div>
+
+      </div>
+  )
+}
+
+export default Header
